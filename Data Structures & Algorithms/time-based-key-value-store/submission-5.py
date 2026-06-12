a7db686fde9091a86718b4be5daf158df83af9ec
rosnMagar <146673489+rosnMagar@users.noreply.github.com>
2026-06-11 21:34:59 -0500

    Add: time-based-key-value-store - submission-5

---
+++ Data Structures & Algorithms/time-based-key-value-store/submission-5.py
@@ -0,0 +1,26 @@
+class TimeMap:
+
+    def __init__(self):
+        self.store = defaultdict(list)
+
+    def set(self, key: str, value: str, timestamp: int) -> None:
+        self.store[key].append((value, timestamp))
+
+    def get(self, key: str, timestamp: int) -> str:
+        lst = self.store[key]
+        res = ""
+        l = 0
+        r = len(lst) - 1
+
+        while l <= r:
+            m = (l + r) // 2
+            if lst[m][1] <= timestamp:
+                res = lst[m][0]
+                l = m + 1
+            else:
+                r = m - 1
+           
+        return res 
+
+
+        
