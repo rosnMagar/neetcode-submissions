473d3ec0f589e8167ba944fbed2f452032b3df3e
rosnMagar <146673489+rosnMagar@users.noreply.github.com>
2026-06-15 21:01:01 -0500

    Add: lru-cache - submission-16

---
+++ Data Structures & Algorithms/lru-cache/submission-16.py
@@ -0,0 +1,46 @@
+from collections import deque
+class ListNode():
+    def __init__(self, key = 0, value = 0, next = None, prev=None):
+        self.value = value
+        self.next = next
+        self.prev = prev
+        self.key = key
+
+class LRUCache:
+    def __init__(self, capacity: int):
+        self.store = {}
+        self.lru = ListNode(0, 0)
+        self.mru = ListNode(0, 0)
+        self.lru.next, self.mru.prev = self.mru, self.lru
+        self.capacity = capacity
+
+    def insert(self, node):
+        prev, nxt = self.mru.prev, self.mru 
+        node.prev, node.next = prev, nxt
+        prev.next = nxt.prev = node
+
+    def delete(self, node):
+        prev, nxt = node.prev, node.next
+        prev.next, nxt.prev = nxt, prev
+
+    def get(self, key: int) -> int:
+        if key not in self.store.keys():
+            return -1
+        
+        self.delete(self.store[key])
+        self.insert(self.store[key])
+
+        return self.store[key].value
+        
+    def put(self, key: int, value: int) -> None:
+        node = ListNode(key, value)
+        if key in self.store.keys():
+            self.delete(self.store[key])
+        
+        self.store[key] = node
+        self.insert(node)
+
+        if len(self.store) > self.capacity:
+            del self.store[self.lru.next.key]
+            self.delete(self.lru.next)
+        
