1b6f17064e3592cba5d7170a77d128ae20d841b9
rosnMagar <146673489+rosnMagar@users.noreply.github.com>
2026-07-08 16:18:24 -0500

    Add: course-schedule - submission-9

---
+++ Data Structures & Algorithms/course-schedule/submission-9.py
@@ -0,0 +1,31 @@
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        adj = {key: [] for key in range(numCourses)}
+        for p in prerequisites:
+            adj[p[0]].append(p[1])
+
+        def rec(i, visit):
+            if i in visit:
+                return False
+            visit.add(i)
+            res = True
+            for j in adj[i]:
+                res = res and rec(j, visit)
+            visit.remove(i)
+            return res
+
+        for k in adj:
+            if not rec(k, set()):
+                return False
+
+        return True   
+            
+
+
+
+
+
+
+
+
+        
